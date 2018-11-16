25d731f661a7948789f70af782fdd95e88c23038
Salvador Ramirez <salvaramirez@iesmarenostrum.com>
2018-11-16 19:15:43 +0100

    añado comas a los ficheros template de tvseries

--- templates/tvseries_template.php
+++ templates/tvseries_template.php
@@ -10,8 +10,8 @@
                 {"rel" : "collection", "href" : "{{ path_for('movies') }}","prompt":"Movies"},
                 {"rel" : "collection", "href" : "{{ path_for('books') }}","prompt":"Books"},
                 {"rel" : "collection", "href" : "{{ path_for('musicalbums') }}","prompt":"Music Albums"},
-                {"rel" : "collection", "href" : "{{ path_for('games') }}","prompt":"Videogames"}
-                {"rel" : "collection", "href" : "{{ path_for('tvseries') }}","prompt":"Tvseries"}
+                {"rel" : "collection", "href" : "{{ path_for('games') }}","prompt":"Videogames"},
+                {"rel" : "collection", "href" : "{{ path_for('tvseries') }}","prompt":"Tvseries"},
             ],
 
             "items" : [
@@ -23,8 +23,8 @@
                             {"name" : "channelPlatform", "value" : "{{ item.channelPlatform }}", "prompt" : "Plataforma de la serie"},
                             {"name" : "category", "value" : "{{ item.category }}", "prompt" : "Categoria del Juego"},
                             {"name" : "seasons", "value" : "{{ item.seasons }}", "prompt" : "Temporadas de la serie"},
-                            {"name" : "language", "value" : "{{ item.language }}", "prompt" : "Idioma de la Serie"}
-                            {"name" : "episodes", "value" : "{{ item.episodes }}", "prompt" : "Numero de episodios"}
+                            {"name" : "language", "value" : "{{ item.language }}", "prompt" : "Idioma de la Serie"},
+                            {"name" : "episodes", "value" : "{{ item.episodes }}", "prompt" : "Numero de episodios"},
                             {"name" : "datePublished", "value" : "{{ item.datePublished }}", "prompt" : "Fecha de lanzamiento"},
 
                         ]
